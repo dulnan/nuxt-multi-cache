f9dbaa422c19b29bb280517bca081b513282c961
Jan Hug <me@dulnan.net>
2022-12-29 11:20:38 +0100

    Fix TS error

--- playground/components/WithAsyncData.vue
+++ playground/components/WithAsyncData.vue
@@ -1,11 +1,11 @@
 <template>
-  <div id="with-async-data">{{ data.api }}</div>
+  <div v-if="data" id="with-async-data">{{ data.api }}</div>
 </template>
 
 <script lang="ts" setup>
 import { useAsyncData } from 'nuxt/app'
 
-const { data } = await useAsyncData('withAsyncData', () => {
+const { data } = await useAsyncData<any>('withAsyncData', () => {
   return $fetch('/api/test').then((v) => {
     return v
   })
