b1004840fd520b92245df246a7b50b8b6bb08b21
Jan Hug <me@dulnan.net>
2024-08-25 14:05:26 +0200

    also test compression on cached api handlers

--- playground/server/plugins/compression.ts
+++ playground/server/plugins/compression.ts
@@ -3,7 +3,10 @@ import { defineNitroPlugin } from 'nitropack/runtime'
 
 export default defineNitroPlugin((nitro) => {
   nitro.hooks.hook('beforeResponse', async (event, response) => {
-    if (!event.path.startsWith('/testCompression')) {
+    if (
+      !event.path.startsWith('/testCompression') &&
+      !event.path.startsWith('/api/testApiCompression')
+    ) {
       return
     }
 
--- test/routeCacheWithCompression.e2e.spec.ts
+++ test/routeCacheWithCompression.e2e.spec.ts
@@ -35,42 +35,50 @@ await setup({
   nuxtConfig,
 })
 
+async function testResponse(path: string) {
+  // First call puts it into cache.
+  const first = await fetch(path, {
+    method: 'get',
+    headers: {
+      'accept-encoding': 'br',
+    },
+  })
+
+  // Get the encoding.
+  const firstEncoding = first.headers.get('content-encoding')
+
+  // Test that the compression feature has actually compressed the response.
+  expect(firstEncoding).toEqual('br')
+
+  // Second call should get it from cache.
+  const second = await fetch(path, {
+    method: 'get',
+    headers: {
+      'accept-encoding': 'br',
+    },
+  })
+
+  const responseFirst = await first.text()
+  const responseSecond = await second.text()
+
+  // Response should be identical (contains a random number).
+  expect(responseFirst).toEqual(responseSecond)
+
+  const secondEncoding = second.headers.get('content-encoding')
+
+  // The encoding should be the same, because the compression feature was
+  // able to compress the cached response again.
+  expect(secondEncoding).toEqual(firstEncoding)
+}
+
 describe.only('The route cache with compression enabled', () => {
   test('caches a page', async () => {
     await purgeAll()
+    await testResponse('/testCompression')
+  })
 
-    // First call puts it into cache.
-    const first = await fetch('/testCompression', {
-      method: 'get',
-      headers: {
-        'accept-encoding': 'br',
-      },
-    })
-
-    // Get the encoding.
-    const firstEncoding = first.headers.get('content-encoding')
-
-    // Test that the compression feature has actually compressed the response.
-    expect(firstEncoding).toEqual('br')
-
-    // Second call should get it from cache.
-    const second = await fetch('/testCompression', {
-      method: 'get',
-      headers: {
-        'accept-encoding': 'br',
-      },
-    })
-
-    const responseFirst = await first.text()
-    const responseSecond = await second.text()
-
-    // Response should be identical (contains a random number).
-    expect(responseFirst).toEqual(responseSecond)
-
-    const secondEncoding = second.headers.get('content-encoding')
-
-    // The encoding should be the same, because the compression feature was
-    // able to compress the cached response again.
-    expect(secondEncoding).toEqual(firstEncoding)
+  test('caches the result of an API handler', async () => {
+    await purgeAll()
+    await testResponse('/api/testApiCompression')
   })
 })
