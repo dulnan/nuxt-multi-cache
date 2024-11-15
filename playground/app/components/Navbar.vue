40b2a1f3e1da7c4673c0d61cc75a2b1edfc295d5
Jan Hug <dulnan@users.noreply.github.com>
2024-11-15 07:42:57 +0100

    fix: max age of 0 should mean uncacheable in useCachedAsyncData (#83)
    * max age 0 should mean uncacheable (#78)

--- playground/app/components/Navbar.vue
+++ playground/app/components/Navbar.vue
@@ -1,5 +1,13 @@
 <template>
   <nav class="menu">
+    <div class="menu-label">Main Menu</div>
+    <ul class="menu-list">
+      <li v-for="route in routes" :key="route.name">
+        <NuxtLink :to="route.path" :id="'route-' + route.name">{{
+          route.name
+        }}</NuxtLink>
+      </li>
+    </ul>
     <div class="menu-label">User Management</div>
     <ul class="menu-list">
       <li v-for="user in users">
@@ -10,11 +18,20 @@
 </template>
 
 <script lang="ts" setup>
-import { useAsyncData } from '#imports'
+import { useAsyncData, useRouter } from '#imports'
 
 const { data: users } = await useAsyncData('navbar', () => {
   return $fetch('/api/getUsers').then((v) => {
     return v
   })
+})
+
+const router = useRouter()
+
+const routes = router.getRoutes().map((route) => {
+  return {
+    name: route.name?.toString() || '',
+    path: route.path,
+  }
 })
 </script>
