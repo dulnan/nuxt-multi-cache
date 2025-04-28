e3008d2ec841f6e3cf789d4de72165a64b070e3b
Jan Hug <me@dulnan.net>
2025-04-28 08:44:42 +0200

    fix: add missing build external

--- build.config.ts
+++ build.config.ts
@@ -2,5 +2,5 @@ import { defineBuildConfig } from 'unbuild'
 
 export default defineBuildConfig({
   entries: ['./src/server-options.ts'],
-  externals: ['unstorage', 'defu', 'h3', 'pathe'],
+  externals: ['unstorage', 'defu', 'h3', 'pathe', 'unplugin'],
 })
