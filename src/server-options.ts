6d47b8e1500ddc9c873f95b9737b4bc063a88a71
Jan Hug <me@dulnan.net>
2025-07-13 10:52:34 +0200

    fix: flip method overloads
    Otherwise the import of the method in projects will be marked as deprecated when it's just a single signature that's deprecated

--- src/server-options.ts
+++ src/server-options.ts
@@ -1,6 +1,14 @@
 import type { MultiCacheServerOptions } from './runtime/types'
 
 /**
+ * Define nuxt-multi-cache server options.
+ */
+export function defineMultiCacheOptions(
+  // eslint-disable-next-line @typescript-eslint/unified-signatures
+  options: () => MultiCacheServerOptions,
+): () => MultiCacheServerOptions
+
+/**
  * Use the signature to pass a method that returns the options instead.
  * @deprecated
  *
@@ -15,14 +23,6 @@ import type { MultiCacheServerOptions } from './runtime/types'
  */
 export function defineMultiCacheOptions(
   options: MultiCacheServerOptions,
-): () => MultiCacheServerOptions
-
-/**
- * Define nuxt-multi-cache server options.
- */
-export function defineMultiCacheOptions(
-  // eslint-disable-next-line @typescript-eslint/unified-signatures
-  options: () => MultiCacheServerOptions,
 ): () => MultiCacheServerOptions
 
 /**
